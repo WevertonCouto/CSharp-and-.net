c2897b8382ce3dca2a390126948a2300e715cbd1
Weverton Couto <wevertoncristais@gmail.com>
2019-12-08 21:40:09 -0300

    Exceções 1

---
+++ CSharpPOO-4/ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            try
+            {
+                int b = 0;
+                double a = (1 / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Impossible");
+            }
+        }
+    }
+}

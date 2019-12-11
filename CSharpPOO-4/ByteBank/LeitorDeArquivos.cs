46cf9e03920486a3e35320140767a8105b4a7e3c
Weverton Couto <wevertoncristais@gmail.com>
2019-12-10 21:45:16 -0300

    Bibliotecas 1

---
+++ CSharpPOO-4/ByteBank/LeitorDeArquivos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class LeitorDeArquivos : IDisposable
+    {
+        public string Arquivo { get; }
+        
+
+        public LeitorDeArquivos(string arquivo)
+        {
+            this.Arquivo = arquivo;
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Arquivo fechado");
+        }
+
+        public void Dispose()
+        {
+            this.Fechar();
+        }
+    }
+}
--- CSharpPOO-4/ByteBank/Program.cs
+++ CSharpPOO-4/ByteBank/Program.cs
@@ -22,6 +22,33 @@ namespace ByteBank
             }
         }
 
+        public static void CarregarContas()
+        {
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("Contas.txt"))
+            {
+
+            }
+            //LeitorDeArquivos leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("Contas.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //catch (System.IO.IOException)
+            //{
+
+            //    throw;
+            //}
+            //finally
+            //{
+            //    if(leitor != null)
+            //        leitor.Fechar();
+            //}
+            
+        }
+
         public static double Dividir(int n1, int n2)
         {
             return n1 / n2;

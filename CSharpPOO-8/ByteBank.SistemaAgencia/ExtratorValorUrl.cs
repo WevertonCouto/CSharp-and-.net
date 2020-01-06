2a6273aeabc2fb33df6f1dc2c6e0e4b0822648d2
Weverton Couto <wevertoncristais@gmail.com>
2020-01-06 14:53:05 -0300

    var e listas genericas

---
+++ CSharpPOO-8/ByteBank.SistemaAgencia/ExtratorValorUrl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class ExtratorValorUrl
+    {
+        public string URL { get; }
+        private readonly string _argumentos;
+        public ExtratorValorUrl(string url)
+        {
+            if (string.IsNullOrEmpty(url)) throw new Exception($"O argumento {nameof(url)} não pode estar vazio ou nullo");
+            this.URL = url;
+
+            int indiceInterrogacao = this.URL.IndexOf('?');
+            _argumentos = URL.Substring(indiceInterrogacao + 1);
+        }
+    }
+}
---
+++ CSharpPOO-8/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            list.Add(1);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.AdicionarVarios(1, 2, 3);
+
+            list.Sort();
+            list.Remove(10);
+            
+
+        }
+    }
+}

0df394ab29991a4174d5774ff28eda68f51ae792
Weverton Couto <wevertoncristais@gmail.com>
2020-01-07 17:13:07 -0300

    Lendo Arquivos

---
+++ CSharpPOO-9/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,37 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var file = "contas.txt";
+            var stream = new FileStream(file, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            var numeroDebytes = -1;
+            while(numeroDebytes != 0)
+            {
+                numeroDebytes = stream.Read(buffer, 0, 1014);
+                EscreverBuffer(buffer);
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var b in buffer)
+            {
+                Console.Write(b);
+            }
+        }
+    }
+} 
+ 

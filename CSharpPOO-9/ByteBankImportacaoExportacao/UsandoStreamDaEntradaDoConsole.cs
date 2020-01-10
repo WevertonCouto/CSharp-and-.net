2f261cf6578f8da77b3117282b5cf334c1a7820a
Weverton Couto <wevertoncristais@gmail.com>
2020-01-10 16:12:24 -0300

    Stream Console

--- CSharpPOO-9/ByteBankImportacaoExportacao/UsandoStreamDaEntradaDoConsole.cs
+++ CSharpPOO-9/ByteBankImportacaoExportacao/UsandoStreamDaEntradaDoConsole.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace ByteBankImportacaoExportacao
 {
     partial class Program
     {
+        public void UsarStreamEntrada()
+        {
+            using (var fluxoEntrada = Console.OpenStandardInput())
+                using (var fs = new FileStream("entradaconsole.txt", FileMode.Create))
+            {
+                var buffer = new byte[1024];
+
+                while (true)
+                {
+                    var byteslidos = fluxoEntrada.Read(buffer, 0, 1024);
+                    fs.Write(buffer, 0, byteslidos);
+                    fs.Flush();
+                    Console.WriteLine(byteslidos);
+                }
+            }
+        }
     }
 }

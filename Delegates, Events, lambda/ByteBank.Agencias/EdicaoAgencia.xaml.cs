10f8320be0979ce4d9a55251f6329cfad5549c42
Weverton Couto <wevertoncristais@gmail.com>
2020-01-21 14:41:40 -0300

    Combine Delegates

---
+++ Delegates, Events, lambda/ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Lógica interna para EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+            this._agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+        }
+
+        public void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+            txtDescricao.Text = _agencia.Descricao;
+        }
+
+        public void AtualizarControles()
+        {
+            btnOK.Click += new RoutedEventHandler(btnOk_Click);
+            btnCancelar.Click += new RoutedEventHandler(btnCancelar_Click);
+
+            btnOK.Click += new RoutedEventHandler(Fechar);
+            btnCancelar.Click += new RoutedEventHandler(Fechar);
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e) => DialogResult = true;
+
+        private void btnOk_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+
+        private void Fechar(object sender, RoutedEventArgs e) => Close();
+    }
+}
